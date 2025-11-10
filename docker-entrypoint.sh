6f84cba4bd71a1faf604feabd3179ba37aa90af4
Woody <2997336+MWG-Logan@users.noreply.github.com>
2025-11-10 10:28:14 -0500

    Add Docker support with entrypoint script and Dockerfile

---
+++ docker-entrypoint.sh
@@ -0,0 +1,12 @@
+﻿#!/bin/bash
+set -e
+
+# If PHP_MEMORY_LIMIT is defined, drop a custom ini
+if [ -n "$PHP_MEMORY_LIMIT" ]; then
+  echo "memory_limit = ${PHP_MEMORY_LIMIT}" \
+    > /usr/local/etc/php/conf.d/custom_php_memory.ini
+  echo "PHP memory_limit set to ${PHP_MEMORY_LIMIT}"
+fi
+
+# Finally, exec the original entrypoint (from the php image) with whatever args
+exec docker-php-entrypoint "$@"
